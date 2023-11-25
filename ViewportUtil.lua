fa07137e688587463ec957f33e71de3492b1f229
Peerless God <91627683+god12l@users.noreply.github.com>
2023-11-24 19:09:06 -0500

    Update ViewportUtil.lua

--- ViewportUtil.lua
+++ ViewportUtil.lua
@@ -8,20 +8,27 @@ local function UpdateCamera (Size, Center, Camera)
     local Depth = MaxSize / math.tan(math.rad(Camera.FieldofView))
   
 Camera.CFrame = CFrame.new(Center.X, Center.Y, Center.Z - Depth) * CFrame.Angles (0, math.pi, 0) 
-  Camera.Focus = Origin
+Camera.Focus = Origin
 end
 
 function Module.Create(Object)
-    assert(Object:ISA("BasePart") or Object: ISA("Model"), string.format("BasePart or Model expected, got %s"), Object.ClassName) Object = Object:Clone()
+    assert(Object:ISA("BasePart") or Object:ISA("Model"), string.format("BasePart or Model expected, got %s"), Object.ClassName) 
+    Object = Object:Clone()
+    
 local Frame = Instance.new("ViewportFrame")
 local Camera = Instance.new("Camera")
 local Size = nil
 local Center = nil
+
+    
+    
 if Object:ISA("BasePart") then
+    Object:ClearAllChildren()
+    Object.CFrame = Object.CFrame.Rotation 
+    Size =Object.Size
+    Center Object.CFrame        
 else
-Object:ClearAllChildren()
-Object.CFrame = Object.CFrame.Rotation Size = Object.Size
-Center Object.CFrame
-Object: PivotTo(Object:GetPivot().Rotation) Size = Object: GetExtentsSize()
+Object: PivotTo(Object:GetPivot().Rotation) 
+Size = Object: GetExtentsSize()
 Center Object: GetBoundingBox()
 end
